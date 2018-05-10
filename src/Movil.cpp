3310f3c7d818e77e39c31b2af454d0014d0cfc1b
cidpau <38727833+cidpau@users.noreply.github.com>
2018-05-10 22:50:31 +0200

    Update Movil.cpp

--- src/Movil.cpp
+++ src/Movil.cpp
@@ -10,8 +10,9 @@ Movil::Movil()
 Movil::~Movil()
 {
 }
-void Movil::Mover() {
-	//ecuaciones físicas
+void Movil::Mueve(float t) {
+	velocidad = velocidad + (0.5*aceleracion.y*t*t);
+	posicion = posicion + velocidad*t;
 }
 Vector2D& Movil::getVelocidad() {
 	return velocidad;
