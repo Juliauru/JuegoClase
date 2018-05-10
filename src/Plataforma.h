6c6eb824723e2161c60233f9f05e188e62c689ab
Juliauru <julia.urusanz@alumnos.upm.es>
2018-05-10 23:50:18 +0200

    Change 2

--- src/Plataforma.h
+++ src/Plataforma.h
@@ -1,22 +1,16 @@
 #pragma once
 #include "Movil.h"
 #include "Pared.h" 
-class Plataforma:public Movil //Cambiar a paredes
+class Plataforma :public Movil, public Pared //Cambiar a paredes
 {
 protected:
-	Pared bases[2];
 	static 	int n_plataformas;
-	int ancho;
-	int largo;
-	float lado;
 public:
-	Plataforma(int l=2);
+	Plataforma(int l = 2);
 	virtual ~Plataforma();
-	void Dibuja();
 	static int get_nplataformas();
 	void setLargo(int l = 2);
-	float Distancia(Vector2D centro, Vector2D* direccion);
-	
 };
+
 
 
