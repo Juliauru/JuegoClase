74c7fa860a05fee2113fe6571cd093002b25155b
cidpau <38727833+cidpau@users.noreply.github.com>
2018-05-10 23:28:07 +0200

    Update Interaccion.cpp

--- src/Interaccion.cpp
+++ src/Interaccion.cpp
@@ -24,3 +24,29 @@ bool Interaccion::Contacto(Movil &m, Plataforma &p) {
 	return false;
 }
 
+void Interaccion::Rebote(Enemigo & e, Enemigo & e1)
+{
+	if (e.posicion.x == e1.posicion.x && e.posicion.y == e1.posicion.y) {
+		e.velocidad.x = -e.velocidad.x;
+		e1.velocidad.x = -e1.velocidad.x;
+	}
+}
+
+bool Interaccion::Rebote(Personaje  &p, Enemigo &e)
+{
+	//IDEA PARA LAS VIDAS
+	//En esta función, si se produce el choque, las vidas del personaje disminuirán en 1. Por tanto, en personaje se creará una variable
+	//estática que será vidas que comenzarán valiendo 3 y en está función irán disminuyendo. En la función bonus aumentarán.
+	//Introduzco friend class en Personaje, tambien función getVida();
+	//ya que cuando se apoyen en las plataformas tendran el mismo punto y, volver a cambiar el if()
+	//Volver a cambiar ((p.posicion.x + (p.altura / 1.5f) > e.posicion.x) && (p.posicion.x - (p.altura / 1.5f) < e.posicion.x) && p.posicion.y == e.posicion.y)
+	if ((p.posicion.x + (p.altura / 1.5f) > e.posicion.x) && (p.posicion.y - (p.altura / 1.5f) < e.posicion.y) && (p.posicion.y + (p.altura / 1.5f) > e.posicion.y) && (p.posicion.x - (p.altura / 1.5f) < e.posicion.x)) {
+		p.vida -= 1;
+		//Esto de las velocidades es una prueba para comprobar visualmente que chocan
+		e.velocidad.x = -e.velocidad.x;
+		return true;
+		//Indicaría que pierde una vida y vuelve a empezar el nivel por el que iba (si la vida es distinta de 0)
+		//Si la vida llega a 0 el programa se acabaría y comenzaría la pantalla principal.
+	}
+	return false;
+}
